17b35fb15e4b2dfd51849a9cec0a0df0fb6d8151
Gustavo-jms36 <gustavo.jms36@gmail.com>
2022-11-13 21:38:35 -0500

    ejercicio 2 sintaxis

---
+++ JavaScript-Basico/primer-proyecto/sintaxisyVariables.js
@@ -0,0 +1,13 @@
+const newList = [
+  "Gustavo",
+  36,
+  true,
+  new Date(1986, 2, 3),
+    {
+      title: "The Perfum"
+      author: "Patrick Suskind",
+      date: new Date(2006, 0, 0),
+      url: "https://www.amazon.com/El-perfume-Historia-asesino-Spanish/dp/843221745X/ref=sr_1_1?crid=12D6GDP3IPROL&keywords=el+perfume+libro&qid=1668393169&sprefix=el+perfume%2Caps%2C246&sr=8-1"
+    }
+
+]
