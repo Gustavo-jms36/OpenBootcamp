a3e3fa118471582b52d503a46fb278935ec484d0
Gustavo-jms36 <gustavo.jms36@gmail.com>
2022-11-17 17:31:28 -0500

    Add Estructuras de Control

---
+++ JavaScript-Basico/Leccion-3 estructuras/factorial-for.js
@@ -0,0 +1,9 @@
+let factorial = 10;
+let result = 1;
+
+for (let i = factorial; i > 0; i--) {
+    result *= i;
+    
+}
+
+console.log(result);
---
+++ JavaScript-Basico/Leccion-3 estructuras/factorial-while.js
@@ -0,0 +1,10 @@
+let factorial = 10;
+let result = 1;
+let i = 1
+
+while (i <= factorial) {
+    result *= i;
+    i++
+}
+
+console.log(result);
