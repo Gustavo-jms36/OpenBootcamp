c55476faa3ac7b4e63366d786d9ca1bcb39b742e
Gustavo-jms36 <gustavo.jms36@gmail.com>
2022-11-17 22:46:37 -0500

    Add metodos de numeros js

---
+++ JavaScript-Basico/leccion-5 metodos de num/metodosNum.js
@@ -0,0 +1,8 @@
+let alturaCm = 176;
+let alturaM = 1.76;
+let pesoKg = 75.6;
+let alturaRedArriba = Math.ceil(alturaM);
+let pesoRedAbajo = Math.floor(alturaM);
+let iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(alturaCm, alturaM, pesoKg, alturaRedArriba, pesoRedAbajo, iguales);
