f58455c1a79c9c31e9d3f79e8d059b68cc9565c5
Gustavo-jms36 <gustavo.jms36@gmail.com>
2022-11-18 22:08:31 -0500

    Add Set Conjuntos

---
+++ JavaScript-Basico/Leccion-7 Sets/conjuntos.js
@@ -0,0 +1,12 @@
+const familia = new Set(
+    [
+        "Gustavo", 
+        "Ani", 
+        "Juan", 
+        "Gustavo"
+]
+    );
+familia.add("Gustavo");
+familia.add("Javascript");
+
+console.log(familia);
